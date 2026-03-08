fc12ce0e39429f31f0b848bdd83503130177c71a
m.peykanfar <m.peykanfar@PEYKANFAR-PC>
2026-03-08 11:41:48 +0330

    افزودن نمایش پیام خطا در گراف کیو ال و افزودن آپدیت پروداکت

--- src/services/Products/Products.Api/GQL/Mutations/ProductsMutations.cs
+++ src/services/Products/Products.Api/GQL/Mutations/ProductsMutations.cs
@@ -4,6 +4,8 @@ using GraphQL.Types;
 using MediatR;
 using Products.Api.GQL.Types.Products;
 using Products.Application.Products.Commands.Create;
+using Products.Application.Products.Commands.Update;
+using Products.Domain.Products;
 
 namespace Products.Api.GQL.Mutations;
 
@@ -19,8 +21,41 @@ public static class ProductsMutations
             resolve: async context =>
             {
                 var addProduct = context.GetArgument<AddProductCommand>("productInput");
-                return await mediator.Send(addProduct);
+                ProductDtos.ProductResDto addProductRes = new();
+                try
+                {
+                    addProductRes = await mediator.Send(addProduct);
+                }
+                catch (Exception ex)
+                {
+                    context.Errors.Add(new ExecutionError(ex.Message));
+                    return null;
+                }
+                return addProductRes;
             }
         );
+        schema.FieldAsync<BooleanGraphType>(
+            "updateProduct",
+            "Is used to update a  product to the database",
+            arguments: new QueryArguments(new QueryArgument<NonNullGraphType<ProductReqType>> { Name = "productInput", Description = "Product input parameter" }),
+
+            resolve: async context =>
+            {
+                UpdateProductCommand updateProduct = context.GetArgument<UpdateProductCommand>("productInput");
+                bool updateProductRes;
+                try
+                {
+                    updateProductRes = await mediator.Send(updateProduct);
+                }
+                catch (Exception ex)
+                {
+
+                    context.Errors.Add(new ExecutionError(ex.Message));
+                    return null;
+
+                }
+
+                return updateProductRes;
+            });
     }
 }
