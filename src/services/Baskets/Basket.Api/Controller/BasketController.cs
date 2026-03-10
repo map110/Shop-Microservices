db704c677042be1e93597b40c1345f65618644aa
m.peykanfar <m.peykanfar@PEYKANFAR-PC>
2026-03-10 15:09:21 +0330

    میکرو سرویس سبد خرید با ردیس

---
+++ src/services/Baskets/Basket.Api/Controller/BasketController.cs
@@ -0,0 +1,42 @@
+﻿using System.Net;
+using Basket.Api.Entities;
+using Basket.Api.Repositories;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Basket.Api.Controller;
+
+[Route("api/[controller]")]
+[ApiController]
+public class BasketController : ControllerBase
+{
+    private readonly IBasketRepository _basketRepository;
+
+    public BasketController(IBasketRepository basketRepository)
+    {
+        _basketRepository = basketRepository ?? throw new ArgumentException(nameof(basketRepository));
+    }
+
+    [HttpGet("{username}", Name = "GetBasket")]
+    [ProducesResponseType(typeof(ShoppingCart), (int)HttpStatusCode.OK)]
+    public async Task<ActionResult<ShoppingCart>> GetBasket(string username)
+    {
+        var basket = await _basketRepository.GetBasket(username);
+        //if basket is null , create new shopping cart
+        return Ok(basket ?? new ShoppingCart(username));
+    }
+
+    [HttpPost]
+    [ProducesResponseType(typeof(ShoppingCart), (int)HttpStatusCode.OK)]
+    public async Task<ActionResult<ShoppingCart>> UpdateBasket(ShoppingCart basket)
+    {
+        return Ok(await _basketRepository.UpdateBasket(basket));
+    }
+
+    [HttpDelete("{userName}", Name = "DeleteBasket")]
+    [ProducesResponseType(typeof(void), (int)HttpStatusCode.OK)]
+    public async Task<IActionResult> DeleteBasket(string userName)
+    {
+        await _basketRepository.DeleteBasket(userName);
+        return Ok();
+    }
+}
