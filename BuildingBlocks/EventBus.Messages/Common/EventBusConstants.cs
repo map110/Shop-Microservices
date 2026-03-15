b844e83825bbcef8074d4a5bf12375a3c06df395
m.peykanfar <m.peykanfar@PEYKANFAR-PC>
2026-03-15 16:19:03 +0330

    افزودن کلاس لایبرری EventBus

---
+++ BuildingBlocks/EventBus.Messages/Common/EventBusConstants.cs
@@ -0,0 +1,6 @@
+﻿namespace EventBus.Messages.Common;
+
+public class EventBusConstants
+{
+    public const string AddProductQueue = "addproduct-queue";
+}
---
+++ BuildingBlocks/EventBus.Messages/Events/AddProductEvent.cs
@@ -0,0 +1,7 @@
+﻿namespace Microsoft.EntityFrameworkCore.Internal;
+
+public class AddProductEvent : IntegrationBaseEvent
+{
+    public int ProductId { get; set; }
+    public string ProductTitle { get; set; }
+}
